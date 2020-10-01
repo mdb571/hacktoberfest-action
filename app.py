0f2d13b7a0d570eab705565e51374f707db84c22
mdb571 <r.mohammedbilal@gmail.com>
2020-10-01 23:56:10 +0530

    defaults

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from datetime import datetime
 
 def add_label(repo: Repository, label_filter,label_to_add: Label):
     """Add hacktoberfest label to all issues labeled with filter label."""
-    if label_filter!="None":
+    if label_filter!=None:
         issues_list = repo.get_issues(state="open",labels=[label_filter])
-    elif label_filter=="None":
+    else:
         issues_list = repo.get_issues(state="open")
     for issue in issues_list:
         issue.add_to_labels(label_to_add)
+    print('Hacktoberfest label added')
 
 def remove_label(repo: Repository):
     """Remove label from all issues with it."""
@@ -30,12 +31,8 @@ def new_label(
         label_to_add = repo.create_label(
             name='hacktoberfest',
             color=label_color,
-            description="Issues for contributing during the period of hacktoberfest"
-        )
+            description="Issues for contributing during the period of hacktoberfest")
     return label_to_add
-
-
-
 
 
 gh_repository = os.getenv("GITHUB_REPOSITORY")
@@ -43,6 +40,8 @@ gh_token = os.getenv("GITHUB_TOKEN")
 label_color = os.getenv("LABEL_COLOR")
 label_filter = os.getenv("LABEL_FILTER")
 
+if label_filter==None:
+    label_filter='FF00FF'
 
 gh = Github(login_or_token=gh_token)
 repo = gh.get_repo(gh_repository)
@@ -50,8 +49,8 @@ repo = gh.get_repo(gh_repository)
 today = datetime.today()
 
 if today.month==10:
-    label_to_add=new_label(repo,label_color)
+    label_to_add=new_label(repo,label_color,)
     add_label(repo, label_filter,label_to_add)
-    print('Hacktoberfest label added')
+   
 else:
     remove_label(repo)
